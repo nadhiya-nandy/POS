6635bb06d8573e8f48eff5935264ec8a43b98113
nadhiya-nandy <nadhiyav@shorepointsys.com>
2020-03-16 18:37:32 +0530

    [FIX]:CREATED VIEW FOR THE MODELS SaleOrder AND SalesProduct

--- addons/pa__sales/__manifest__.py
+++ addons/pa__sales/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/pa_sales_product.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
